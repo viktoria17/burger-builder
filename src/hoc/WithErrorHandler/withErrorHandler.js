2b41480d36368f6a8c4808b4c830a667a67e1b7b
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2019-11-28 18:42:30 +0200

    Remove interceptors

--- src/hoc/WithErrorHandler/withErrorHandler.js
+++ src/hoc/WithErrorHandler/withErrorHandler.js
@@ -10,14 +10,14 @@ const withErrorHandler = (WrappedComponent, axios) => {
 		};
 
 		componentWillMount() {
-			axios.interceptors.request.use(req => {
+			this.reqInterceptor = axios.interceptors.request.use(req => {
 				this.setState({
 					error: null
 				});
 
 				return req;
 			});
-			axios.interceptors.response.use(
+			this.resInterceptor = axios.interceptors.response.use(
 				res => res,
 				error => {
 					this.setState({
@@ -25,6 +25,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
 					});
 				}
 			);
+		}
+
+		componentWillUnmount() {
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.request.eject(this.resInterceptor);
 		}
 
 		errorConfirmedHandler = () => {
