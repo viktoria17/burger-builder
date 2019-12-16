5e1aa5b3ae789a4e355ddcda8809a590f6ecc094
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2019-12-16 22:03:14 +0200

    Pass ingredients via Query Params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
@@ -10,6 +10,18 @@ class Checkout extends Component {
 			bacon: 1
 		}
 	};
+
+	componentDidMount() {
+		const query = new URLSearchParams(this.props.location.search);
+		const ingredients = {};
+
+		for (let param of query.entries()) {
+			// ['salad', '1']
+			ingredients[param[0]] = +param[1];
+		}
+
+		this.setState({ingredients: ingredients});
+	}
 
 	checkoutCanceledHandler = () => {
 		this.props.history.goBack();
