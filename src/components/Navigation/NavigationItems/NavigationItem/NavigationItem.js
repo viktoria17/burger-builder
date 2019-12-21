6b84c0c44abf7b67cd81cfb7a5dd466fc6dfca49
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2019-12-21 20:32:30 +0200

    Add Orders page and Navigation links

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
-const navigationItem = ({ link, children, active }) => (
+const navigationItem = ({ link, children, exact }) => (
 	<li className={classes.NavigationItem}>
-		<a
-			className={active ? 'active' : null}
-			href={link}
-		>{children}</a>
+		<NavLink
+			activeClassName={classes.active}
+			to={link}
+			exact={exact}
+		>{children}</NavLink>
 	</li>
 );
 
